6980396381d00e4f49554f55e2fdb816cfefa10b
Kanduri Abhinay <kanduriabhinay@gmail.com>
2023-01-28 15:54:42 +0530

    api call on home

--- food-ordering-app/src/Home.js
+++ food-ordering-app/src/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./styles/Home.css";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { updateData } from "./store/Actions";
+
 const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    fetchData();
+  }, []);
+  async function fetchData() {
+    let resp = await fetch(
+      "https://food-ordering-app-server.herokuapp.com/getItems"
+    );
+    resp = await resp.json();
+    dispatch(updateData(resp));
+  }
   return (
     <div className="home-container">
       <Link to="/admin">
